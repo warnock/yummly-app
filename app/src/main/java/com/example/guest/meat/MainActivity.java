2b6d20e60ad23ed09c79f0739005a4f8102f5e48
Megan Warnock <megandwarnock@gmail.com>
2017-05-26 09:26:53 -0700

    add ButterKnife dependency

--- app/src/main/java/com/example/guest/meat/MainActivity.java
+++ app/src/main/java/com/example/guest/meat/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.guest.meat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.findRecipeButton) Button mFindRecipeButton;
+    @Bind(R.id.typeOfMeat) EditText mTypeOfMeat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
