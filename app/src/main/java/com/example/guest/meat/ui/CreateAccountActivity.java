a3a1398a0c4d47bf58f53f81e2a9e105a7469cb5
Megan Warnock <megandwarnock@gmail.com>
2017-06-09 13:57:26 -0700

    add create account, login activities and xml layouts

---
+++ app/src/main/java/com/example/guest/meat/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.meat.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.meat.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
